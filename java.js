aa541a59e12be478d03a25afcd2b1e488798a0b6
rabbihasan01975741945 <rabbihasan01975741945@gmail.com>
2023-08-19 19:37:30 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,7 @@
+// First problem------------->
+function cubeNumber(number){
+    const cube_of_anynumber = Math.pow(number, 3);
+    return cube_of_anynumber;
+}
+const output = cubeNumber(4);
+console.log(output);
